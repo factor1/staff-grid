f4718b08d06b5be57faeb8268070c2884c14d4f5
Matthew Adams <mattadams@factor1studios.com>
2015-01-05 15:32:50 -0700

    updated CSS and examples

--- staff-grid/example_output.php
+++ staff-grid/example_output.php
@@ -28,66 +28,82 @@
 <h3>Leadership</h3>
 
 		
-		<?php
-global $post;
-$args = array( 
-		'numberposts' 			=> -1, 
-		'post_type'              => 'f1_staffgrid_cpt',	
-		//'f1_staffgrid_tax' 		=> 'leadership', // Department Taxonomy (per site)
-		'meta_key' 				=> 'last_name',
-		'orderby'					=> 'meta_value', 
-		'order' 						=> 'ASC'
-		);
+	<ul class="medium-block-grid-3">
+					<?php
+					global $post;
+					$args = array( 
+							'numberposts' 			=> -1, 
+							'post_type'              => 'f1_staffgrid_cpt',	
+							//'f1_staffgrid_tax' 		=> 'leadership', // Department Taxonomy (per site).
+							'meta_key' 				=> 'last_name',
+							'orderby'					=> 'meta_value', 
+							//'post__in' 				=> array( 2, 5, 12, 14, 20 );,  // Use ID's in an array for specific ordering.
+							'order' 						=> 'ASC'
+							
+							);
 		
-$staffgrid = get_posts( $args );
-foreach( $staffgrid as $post ) :	setup_postdata($post); ?>
+					$staffgrid = get_posts( $args );
+					foreach( $staffgrid as $post ) :	setup_postdata($post); ?>
+					
+					<li>
+						<a data-remodal-target="modal-<?php the_ID(); ?>" style="text-align:center">
+						
+							<div class="f1_mentor_photo_container">
+							<?php if(has_post_thumbnail()) {
+								the_post_thumbnail();
+								} else {	}
+							?>
+							</div>
+						
+							<div class="f1_mentor_summary_name"><strong><?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?></strong></div>
+							<div class="f1_mentor_summary_title"><small><?php if(get_field( "title" )) : the_field( "title" ); endif; ?></small></div>
+					</a>
+					</li>
+					
+					
+					<div class="remodal" data-remodal-id="modal-<?php the_ID();?>">
+
+						<div class="row">
+							<div class="medium-4 columns">
+								<div class="f1_mentor_details_photo_container">
+								<?php if(has_post_thumbnail()) {
+									the_post_thumbnail();
+									} else {	}
+									?>
+								</div>
+								
+								<div class="f1_mentor_details_social_container">
+									<?php if(get_field( "phone" )) : echo('Phone: '); the_field( "phone" ); endif; ?></br>
+									<?php if(get_field( "email_address" )) : echo('<a href="'); the_field( "email_address"); echo('">Email '); the_field( "first_name" ); echo('</a></br>'); endif; ?>
+									
+									<?php if(get_field( "twitter_url" )) : echo('<a href="'); the_field( "twitter_url" ); echo('">Twitter</a></br>');  endif; ?>
+									<?php if(get_field( "facebook_url" )) : echo('<a href="'); the_field( "facebook_url" ); echo('">Facebook</a></br>');  endif; ?>
+									<?php if(get_field( "linkedin_irl" )) : echo('<a href="'); the_field( "linkedin_irl" ); echo('">LinkedIn</a></br>');  endif; ?>
+									<?php if(get_field( "instagram_url" )) : echo('<a href="'); the_field( "instagram_url" ); echo('">Instagram</a></br>');  endif; ?>
+									
+	
+								</div>
+							
+							</div>
+							
+							<div class="medium-8 columns">
+									<p class="f1_mentor_details_name"><strong><?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?></strong></br>
+									<span class="f1_mentor_summary_title"><small>	<?php if(get_field( "title" )) : the_field( "title" ); endif; ?></small></span></p>
+									
+									<div class="f1_mentor_details_bio">
+									<?php if(get_field( "staff_bio" )) : the_field( "staff_bio" ); endif; ?>
+									</div>
+							</div>
+						</div>
+					</div>
 
 
-<h2><a data-remodal-target="modal-<?php the_ID(); ?>"><?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?></a></h2>
-
-<div class="remodal" data-remodal-id="modal-<?php the_ID();?>">
-
-	<div class="row">
-		<div class="medium-4 columns">
-			<?php if(has_post_thumbnail()) {
-			the_post_thumbnail();
-			} else {	}
-			?>
-			<?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?>
-		</div>
-		
-		<div class="medium-8 columns">
-			<dl>
-					<dt>Position</dt>
-					<dd><?php if(get_field( "title" )) : the_field( "title" ); endif; ?></dd>
-					
-					<dt>Phone</dt>
-					<dd><?php if(get_field( "phone" )) : the_field( "phone" ); endif; ?></dd>
-					
-					<dt>Email</dt>
-					<dd><?php if(get_field( "email_address" )) : the_field( "email_address" ); endif; ?></dd>
-					
-					<dt>Bio</dt>
-					<dd><?php if(get_field( "staff_bio" )) : the_field( "staff_bio" ); endif; ?></dd>
-					
-					<dt>Twitter</dt>
-					<dd><?php if(get_field( "twitter_url" )) : the_field( "twitter_url" ); else: echo('none');  endif; ?></dd>
-					
-					<dt>Facebook</dt>
-					<dd><?php if(get_field( "facebook_url" )) : the_field( "facebook_url" ); else: echo('none');  endif; ?></dd>
-					
-					<dt>LinkedIn</dt>
-					<dd><?php if(get_field( "twitter_url" )) : the_field( "linkedin_url" ); else: echo('none');  endif; ?></dd>
-					
-					<dt>Instagram</dt>
-					<dd><?php if(get_field( "instagram_url" )) : the_field( "instagram_url" ); else: echo('none');  endif; ?></dd>
-			</dl>
-		</div>
-	</div>
-</div>
+					<?php endforeach; ?>
+			</ul>
 
 
-<?php endforeach; ?>
+
+
 </div>
 
 
