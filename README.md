87a5a0fe8b6e11c5c7c745234b3afd6ff2178342
Matthew Adams <mattadams@factor1studios.com>
2015-01-05 11:33:44 -0700

    Adding some working examples

--- README.md
+++ README.md
@@ -10,9 +10,33 @@ This plugin is pretty ready to roll as is. But to implement the staff output, we
 ### WordPress Query Code Examples ###
 
 
-		// WP_Query arguments
-		$args = array (
-			'post_type'              => 'f1_staffgrid_cpt',
-			'pagination'             => false,
-			'orderby'                => 'menu_order',
-		);
+	// WP_Query arguments
+		global $post;
+			$args = array( 
+			'numberposts' 			=> -1, 
+			'post_type'              => 'f1_staffgrid_cpt',	
+			//'f1_staffgrid_tax' 		=> 'leadership', // Department Taxonomy (per site)
+			'meta_key' 				=> 'last_name',
+			'orderby'					=> 'meta_value', 
+			'order' 						=> 'ASC'
+			);
+		
+		$staffgrid = get_posts( $args );
+		foreach( $staffgrid as $post ) :	setup_postdata($post); 
+
+### Default Staff Grid ACF Fields ###
+Position:  the_field( "title" );
+	
+Phone:  the_field( "phone" );
+	
+Email:  the_field( "email_address" );
+	
+Bio:  the_field( "staff_bio" );
+	
+Twitter:  the_field( "twitter_url" );
+	
+Facebook:  the_field( "facebook_url" );
+	
+LinkedIn:  the_field( "twitter_url" );
+	
+Instagram:  the_field( "instagram_url" );
---
+++ staff-grid/example_output.php
@@ -0,0 +1,86 @@
+<?php 
+	
+	/* Template Name: Staff */
+	get_header(); ?>
+
+<article>
+	<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+		
+		<div class="post" id="post-<?php the_ID(); ?>">
+	
+		<?php if(has_post_thumbnail()) {
+			the_post_thumbnail();
+			} else {	}
+			?>
+			
+			<h1><?php the_title(); ?></h1>
+			<?php the_content(); ?>
+			
+
+			<?php edit_post_link('Edit this entry.', '<hr><p>', '</p>'); ?>
+		</div>
+		
+		<?php endwhile; endif; ?>
+
+
+
+<div class="post row">
+<h3>Leadership</h3>
+
+		
+		<?php
+global $post;
+$args = array( 
+		'numberposts' 			=> -1, 
+		'post_type'              => 'f1_staffgrid_cpt',	
+		//'f1_staffgrid_tax' 		=> 'leadership', // Department Taxonomy (per site)
+		'meta_key' 				=> 'last_name',
+		'orderby'					=> 'meta_value', 
+		'order' 						=> 'ASC'
+		);
+		
+$staffgrid = get_posts( $args );
+foreach( $staffgrid as $post ) :	setup_postdata($post); ?>
+
+
+<h2><?php the_field( "first_name" ); ?>&nbsp;<?php the_field( "last_name" ); ?></h2>
+
+<dl>
+	<dt>Position</dt>
+	<dd><?php if(get_field( "title" )) : the_field( "title" ); endif; ?></dd>
+	
+	<dt>Phone</dt>
+	<dd><?php if(get_field( "phone" )) : the_field( "phone" ); endif; ?></dd>
+	
+	<dt>Email</dt>
+	<dd><?php if(get_field( "email_address" )) : the_field( "email_address" ); endif; ?></dd>
+	
+	<dt>Bio</dt>
+	<dd><?php if(get_field( "staff_bio" )) : the_field( "staff_bio" ); endif; ?></dd>
+	
+	<dt>Twitter</dt>
+	<dd><?php if(get_field( "twitter_url" )) : the_field( "twitter_url" ); else: echo('none');  endif; ?></dd>
+	
+	<dt>Facebook</dt>
+	<dd><?php if(get_field( "facebook_url" )) : the_field( "facebook_url" ); else: echo('none');  endif; ?></dd>
+	
+	<dt>LinkedIn</dt>
+	<dd><?php if(get_field( "twitter_url" )) : the_field( "linkedin_url" ); else: echo('none');  endif; ?></dd>
+	
+	<dt>Instagram</dt>
+	<dd><?php if(get_field( "instagram_url" )) : the_field( "instagram_url" ); else: echo('none');  endif; ?></dd>
+	
+	
+</dl>
+
+
+<?php endforeach; ?>
+</div>
+
+
+
+
+</article>
+
+
+<?php get_footer(); ?>
