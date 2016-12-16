6ea9a6dfa40832504ef76592db8c6506a7c193bd
Eric Stout <ericwstout@gmail.com>
2016-12-16 09:22:47 -0700

    Updates gitignore, updates gulpfile, adds dependencies, builds plugin zip

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const zip = require('gulp-zip');
 
 gulp.task('default', () => {
-  return gulp.src('src/staff-grid-plugin/**/*')
+  return gulp.src('src/staff-grid/**/*')
     .pipe(zip('staff-grid-plugin.zip'))
     .pipe(gulp.dest('dist'));
 });
